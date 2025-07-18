f7381ec914a333fbe98ef9a7f8d80c476d7e4da2
TrieuThien <trieuthien026@gmail.com>
2025-07-18 15:33:09 +0700

    Add HandleData.php

---
+++ app/models/HandleData.php
@@ -0,0 +1,26 @@
+<?php
+    require_once '../../config/Database.php';
+
+    class HandleData extends Database {
+        private $db;
+
+        public function __construct() {
+            $this->db = new Database();
+        }
+
+        // Hàm thực thi trả về dữ liệu
+        public function getData($sql) {
+            $conn = $this->db->connectDB();
+            $stmt = $conn->prepare($sql);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        // Hàm thực thi không trả về dữ liệu
+        public function execData($sql) {
+            $conn = $this->db->connectDB();
+            $conn->query($sql);
+        }
+    }
+
+?>
