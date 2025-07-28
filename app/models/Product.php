6156948641c4f449b03d219324b5183e58e91b09
congbang04 <bangle161004@gmail.com>
2025-07-28 08:38:43 +0700

    done product detail

--- app/models/Product.php
+++ app/models/Product.php
@@ -36,9 +36,9 @@ class Product
             LEFT JOIN productimage pi ON p.ProductID = pi.ProductID
             WHERE (pi.IsThumbnail = 1 OR pi.IsThumbnail IS NULL)
             AND (p.ProductName LIKE :keyword OR p.Brand LIKE :keyword)";
-
+        $params = ['keyword' => $keyword];
         // Truyền giá trị cho :keyword
-        $result = $handleData->getDataWithParams($sql, ['keyword' => $keyword]);
+        $result = $handleData->getDataWithParams($sql, $params);
 
         return $result;
     }
@@ -46,9 +46,12 @@ class Product
     function getProductById($productId)
     {
         $handleData = new HandleData();
-        $sql = "SELECT p.*, pi.ImageURL
+        $sql = "SELECT p.*, pi.ImageURL, s.ShopName, r.Rating, c.CategoryName
             FROM product p
             LEFT JOIN productimage pi ON p.ProductID = pi.ProductID
+            LEFT JOIN shop s ON p.ShopID = s.ShopID
+            LEFT JOIN review r ON p.ProductID = r.ProductID
+            LEFT JOIN category c ON p.CategoryID = c.CategoryID
             WHERE p.ProductID = :productId";
         $params = ['productId' => $productId];
         $result = $handleData->getDataWithParams($sql, $params);
@@ -60,5 +63,99 @@ class Product
 
         return null; // Không tìm thấy sản phẩm
     }
+    public function updateRatingProduct($productId)
+    {
+        $handleData = new HandleData();
+
+        $sql = "SELECT AVG(Rating) AS avg_rating FROM review WHERE ProductID = :productId";
+        $params = ['productId' => $productId];
+        $avgRating = $handleData->getDataWithParams($sql, $params);
+
+
+        $updateSql = "UPDATE product SET RatingProduct = :avg WHERE ProductID = :productId";
+        $params = [
+            'avg' => $avgRating[0]['avg_rating'],
+            'productId' => $productId
+        ];
+        $result = $handleData->getDataWithParams($updateSql, $params);
+
+        // Kiểm tra nếu có kết quả trả về
+        if (count($result) > 0) {
+            return $result[0]; // Trả về sản phẩm đầu tiên
+        }
+
+        return null;
+    }
+    public function countReviewProduct($productId)
+    {
+        $handleData = new HandleData();
+        $sql = "SELECT COUNT(ReviewID) AS count FROM review WHERE ProductID = :productId";
+        $params = ['productId' => $productId];
+        $result = $handleData->getDataWithParams($sql, $params);
+
+        if (count($result) > 0) {
+            return $result[0]['count']; // Trả về số lượng đánh giá
+        }
+
+        return 0; // Không có đánh giá nào
+    }
+
+    public function getReviewComment($productId)
+    {
+        $handleData = new HandleData();
+        $sql = "SELECT r.*, c.FullName
+            FROM review r
+            LEFT JOIN customer c ON r.UserID = c.UserID
+            WHERE r.ProductID = :productId";
+        $params = ['productId' => $productId];
+        $result = $handleData->getDataWithParams($sql, $params);
+
+        return $result; // Trả về danh sách đánh giá
+    }
+    public function addReviewComment($rating, $comment, $productId, $shopId, $userId)
+    {
+        $handleData = new HandleData();
+        // Thêm đánh giá vào cơ sở dữ liệu
+        $sql = "INSERT INTO review (Rating, Comment, CreateAt, UserID, ProductID, ShopID) VALUES (:rating, :comment, NOW(),:userId,:productId, :shopId)";
+        $params = [
+            'productId' => $productId,
+            'userId' => $userId,
+            'rating' => $rating,
+            'comment' => $comment,
+            'shopId' => $shopId
+        ];
+        $handleData->execDataWithParams($sql, $params);
+
+        // Cập nhật lại rating trung bình của sản phẩm
+        $this->updateRatingProduct($productId);
+
+    }
+    public function getRelatedProduct($categoryId)
+    {
+        $handleData = new HandleData();
+        $sql = "SELECT p.*, pi.ImageURL
+            FROM product p
+            LEFT JOIN productimage pi ON p.ProductID = pi.ProductID
+            WHERE p.CategoryId = :categoryId";
+        $params = ['categoryId' => $categoryId];
+        $result = $handleData->getDataWithParams($sql, $params);
+
+        return $result ?? []; // Trả về mảng, tránh lỗi null
+
+    }
+
+    // cập nhật đánh giá của shop
+    // public function updateRatingShop($shopId)
+    // {
+    //     $sql = "SELECT AVG(RatingProduct) AS avg_rating FROM PRODUCT WHERE ShopID = :shopId";
+    //     $stmt = $this->db->prepare($sql);
+    //     $stmt->execute(['shopId' => $shopId]);
+    //     $avg = $stmt->fetchColumn();
+
+    //     $updateSql = "UPDATE SHOP SET RatingShop = :avg WHERE ShopID = :shopId";
+    //     $updateStmt = $this->db->prepare($updateSql);
+    //     $updateStmt->execute(['avg' => $avg, 'shopId' => $shopId]);
+    // }
+
 }
 ?>
