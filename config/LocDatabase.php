2dfabaf9234fe3a50bcbda100d6610aadf7efc69
TrieuThien <trieuthien026@gmail.com>
2025-07-31 17:11:31 +0700

    Fix: Config LocDatabase.php

--- config/LocDatabase.php
+++ config/LocDatabase.php
@@ -1,27 +1,36 @@
 <?php
-    class LocDatabase {
-        private $servername;
-        private $username;
-        private $password;
-        private $dbname;
+class LocDatabase
+{
+    private $servername;
+    private $username;
+    private $password;
+    private $dbname;
 
-        public function __construct() {
+    public function __construct()
+    {
         $this->servername = $_ENV['DB_HOST'];
         $this->username = $_ENV['DB_USER'];
         $this->password = $_ENV['DB_PASS'];
         $this->dbname = $_ENV['LOC_DB_NAME'];
     }
 
-        function connectDB() {
-            try {
-                $conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname;charset=utf8", $this->username, $this->password);
-                // Thiết lập chế độ báo lỗi
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                return $conn;
-            } catch (PDOException $e) {
-                echo "Error: Không thể kết nối CSDL: " . $e->getMessage();
-            }
+    function connectDB()
+    {
+        try {
+            $conn = new PDO(
+                "mysql:host=$this->servername;port=25060;dbname=$this->dbname;charset=utf8",
+                $this->username,
+                $this->password,
+                [
+                    PDO::MYSQL_ATTR_SSL_CA => __DIR__ . "/../ca-certificate.crt", // đặt đúng đường dẫn
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+                ]
+            );
+            return $conn;
+        } catch (PDOException $e) {
+            echo "Error: Không thể kết nối CSDL: " . $e->getMessage();
         }
+    }
 
 }
 // Khai báo lớp Database
